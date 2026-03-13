2a1b22d261ea655441204d0f0eeb6ceff0481030
Adriano Candido <analista.adrianolima@gmail.com>
2026-03-13 19:01:13 -0300

    aula 03-09

---
+++ backend/src/main/java/br/edu/christus/backend/domain/User.java
@@ -0,0 +1,28 @@
+package br.edu.christus.backend.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 150, nullable = false)
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String login;
+    private String password;
+
+}
