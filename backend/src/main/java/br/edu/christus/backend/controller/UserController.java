96aad61b0174bad3c7d85acf7b6bc7d820c2c742
Adriano Candido <analista.adrianolima@gmail.com>
2026-03-16 20:39:53 -0300

    aula 16-03

--- backend/src/main/java/br/edu/christus/backend/controller/UserController.java
+++ backend/src/main/java/br/edu/christus/backend/controller/UserController.java
@@ -1,4 +1,23 @@
 package br.edu.christus.backend.controller;
 
+import br.edu.christus.backend.domain.User;
+import br.edu.christus.backend.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    @Autowired
+    private UserService service;
+
+    @PostMapping
+    public User create(@RequestBody User user){
+        return service.save(user);
+    }
+
 }
--- backend/src/main/java/br/edu/christus/backend/service/UserService.java
+++ backend/src/main/java/br/edu/christus/backend/service/UserService.java
@@ -1,4 +1,25 @@
 package br.edu.christus.backend.service;
 
+import br.edu.christus.backend.domain.User;
+import br.edu.christus.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public User save(User user){
+        if(user.getName().length() > 150){
+            System.out.println("Não é permitido nomes com mais de 50 caracteres");
+            return null;
+        }
+
+        var userSaved = repository.save(user);
+
+        return userSaved;
+    }
+
 }
