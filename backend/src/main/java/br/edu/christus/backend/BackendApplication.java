c2c26187d1c7ba12b61d57796a4ca422cc7ab668
Adriano Candido <analista.adrianolima@gmail.com>
2026-04-25 14:37:15 -0300

    aula 24-04

--- backend/src/main/java/br/edu/christus/backend/BackendApplication.java
+++ backend/src/main/java/br/edu/christus/backend/BackendApplication.java
@@ -3,6 +3,22 @@ package br.edu.christus.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Backend - API User-Address",
+				version = "1.0",
+				description = "Trata-se de uma API construída nas aulas " +
+						"da disciplina TEPW",
+				contact = @Contact(
+						name = "Adriano Lima",
+						email = "analista.adrianolima@gmail.com",
+						url = "https://www.exemplo.com")
+		)
+)
 @SpringBootApplication
 public class BackendApplication {
 
--- backend/src/main/java/br/edu/christus/backend/controller/UserController.java
+++ backend/src/main/java/br/edu/christus/backend/controller/UserController.java
@@ -4,6 +4,11 @@ import br.edu.christus.backend.domain.dto.UserDTO;
 import br.edu.christus.backend.domain.dto.UserLowDTO;
 import br.edu.christus.backend.domain.model.User;
 import br.edu.christus.backend.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,6 +21,7 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Operation(summary = "Cadastra os dados de um usuário | role: [ADMIN]", tags = "User")
     @PostMapping
     public UserLowDTO create(@RequestBody UserDTO user){
         return service.save(user);
@@ -31,6 +37,24 @@ public class UserController {
         return service.findAll();
     }
 
+    @Operation(summary = "Retorna os dados de um usuário | role: [ADMIN]", tags = "User")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", description = "Usuário retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Usuário não encontrado",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(type = "object",
+                                    example = """
+            {
+              "timestamp": "string",
+              "status": 404,
+              "error": "string",
+              "message": "string",
+              "path": "string"
+            }
+            """)
+                    ))
+    })
     @GetMapping("/{id}")
     public UserDTO findById(@PathVariable(name = "id") Long id){
         return service.findById(id);
